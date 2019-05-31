6fb12346b53e993ddb8415b81e9774727c0e82dd
Alejandro <gomez.ruiz.alejandro3@gmail.com>
2019-05-31 13:23:39 +0200

    welcome

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+// import Home from "./views/Home.vue";
 import Test from "./views/Test.vue";
 import Welcome from "./views/Welcome.vue";
 import Home2 from "./views/Home2.vue";
@@ -17,11 +17,11 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: Home
+    // },
 
     {
       path: "/test",
@@ -29,7 +29,7 @@ export default new Router({
       component: Test
     },
     {
-      path: "/welcome",
+      path: "/",
       name: "welome",
       component: Welcome
     },
